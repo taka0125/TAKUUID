352ee194a2040e87e7ded0336218a8e2ab499892
Takahiro Ooishi <taka0125@gmail.com>
2014-06-11 12:12:34 +0900

    update sample

--- Examples/TAKUUIDSample/TAKViewController.m
+++ Examples/TAKUUIDSample/TAKViewController.m
@@ -6,16 +6,19 @@
 - (IBAction)remove {
   BOOL result = [[TAKUUIDStorage sharedInstance] remove];
   NSLog(@"remove => %d", result);
+  NSLog(@"lastErrorStatus = %d", (int)[TAKUUIDStorage sharedInstance].lastErrorStatus);
 }
 
 - (IBAction)findOrCreate {
   NSString *UUIDString = [[TAKUUIDStorage sharedInstance] findOrCreate];
   NSLog(@"UUID = %@", UUIDString);
+  NSLog(@"lastErrorStatus = %d", (int)[TAKUUIDStorage sharedInstance].lastErrorStatus);
 }
 
 - (IBAction)renew {
   NSString *UUIDString = [[TAKUUIDStorage sharedInstance] renew];
   NSLog(@"UUID = %@", UUIDString);
+  NSLog(@"lastErrorStatus = %d", (int)[TAKUUIDStorage sharedInstance].lastErrorStatus);
 }
 
 @end
