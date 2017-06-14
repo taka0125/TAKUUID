43b34ef6265641529ff87499ea6019f7e82363e6
Koji Murata <koji.murata@dena.com>
2017-06-14 14:51:30 +0900

    work via carthage

---
+++ Carthage/TAKUUID/TAKUUID/TAKUUID.h
@@ -0,0 +1,19 @@
+//
+//  TAKUUID.h
+//  TAKUUID
+//
+//  Created by Koji Murata on 2017/06/14.
+//  Copyright © 2017年 Koji Murata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TAKUUID.
+FOUNDATION_EXPORT double TAKUUIDVersionNumber;
+
+//! Project version string for TAKUUID.
+FOUNDATION_EXPORT const unsigned char TAKUUIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TAKUUID/PublicHeader.h>
+
+#import <TAKUUID/TAKUUIDStorage.h>
